c9d71c8737bdef0c44695fb5ddfec83f02b78a59
nettaku <nettaku@naver.com>
2021-10-26 20:54:36 +0900

    react second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Hello from './components/Hello'
+import axios from 'axios';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      const result = await axios('https://jsonplaceholder.typicode.com/posts');
+    setPosts(result.data);
+    }
+
+    getPosts();
+  });
+
   return (
-    <Hello />
+    <div>
+      <Button variant="primary">버튼</Button>
+      <ul>
+        {posts.map(post => <li key={post.id}>{post.title}</li>)}
+      </ul>
+    </div>
   );
 }
 
