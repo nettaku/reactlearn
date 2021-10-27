5b834ff3a1493658c873f681a95d9cd2cea77a92
Harry <choyonghee1974@gmail.com>
2021-10-27 14:49:51 +0900

    interesting!

--- src/App.js
+++ src/App.js
@@ -2,42 +2,83 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   // const [posts, setPosts] = useState([]);
-  const [products, setProducts] = useState([]);
+  const [data, setData] = useState([]);
+  const [model, setModel] = useState("model");
+  const [id, setId] = useState("id");
 
-  useEffect(() => {
-    async function getPosts() {
-      // const result = await axios('https://jsonplaceholder.typicode.com/posts');
-      const result = await axios('http://localhost:8000/store/products');
-    // setPosts(result.data);
-    setProducts(result.data);
-    }
+  const handleSelect = e => {
+    setModel(e.target.value);
+  }
 
-    getPosts();
-  }, []);
+  const handleInput = e => {
+    setId(e.target.value);
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    let endpoint = 'http://localhost:8000/store/';
+    endpoint = id === 'all' ? endpoint + model: endpoint + model + '/' + id
+    const result = await axios(endpoint);    
+    setData(Array.isArray(result.data) ? result.data : [result.data])
+  }
+
+  // useEffect(() => {
+  //   async function getPosts() {
+  //     // const result = await axios('https://jsonplaceholder.typicode.com/posts');
+  //     const result = await axios('http://localhost:8000/store/products');
+  //   // setPosts(result.data);
+  //   setProducts(result.data);
+  //   }
+
+  //   getPosts();
+  // }, []);
 
   return (
+    <>
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col>
+      <Form.Select aria-label="Default select example" name="model" onChange={handleSelect}>
+        <option>Select model</option>
+        <option value="products">Products</option>
+        <option value="collections">Collections</option>
+      </Form.Select>
+      </Col>
+      <Col>
+      <Form.Control type="text" name="id" placeholder="Enter the id or 'all'" onChange={handleInput}/>
+      </Col>
+      <Col>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      </Col>
+      </Row>
+    </Form>
     <Table striped bordered hover size="sm">
       <thead>
         <tr>
           <th>ID</th>
           <th>Title</th>
-          <th>Price</th>
         </tr>
       </thead>
       <tbody>
-        {products.map(product => 
-          <tr key={product.id}>
-            <td>{product.id}</td>
-            <td>{product.title}</td>
-            <td>{product.unit_price}</td>
+        {data.map(datum => 
+          <tr key={datum.id}>
+            <td>{datum.id}</td>
+            <td>{datum.title}</td>            
           </tr>
         )}
       </tbody>
     </Table>
+  </>
   );
 }
 
